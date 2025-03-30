a171aa4e6ffc4b9c49f87384fcc3db81bea65056
yuitaa <95457974+yuitaa@users.noreply.github.com>
2025-03-30 13:20:15 +0900

    config.tsの変更

--- src/config.ts
+++ src/config.ts
@@ -7,12 +7,12 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  title: 'koteiou.pages.dev',
+  subtitle: 'yuita のブログ',
+  lang: 'ja',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 190,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: true,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: false,
@@ -28,55 +28,72 @@ export const siteConfig: SiteConfig = {
     enable: true,           // Display the table of contents on the right side of the post
     depth: 2                // Maximum heading depth to show in the table, from 1 to 3
   },
-  favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+  favicon: [
+    {
+      src: '/favicon/favicon.svg'
+    }
   ]
 }
 
 export const navBarConfig: NavBarConfig = {
   links: [
     LinkPreset.Home,
-    LinkPreset.Archive,
     LinkPreset.About,
     {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      name: '🐦 Twitter',
+      url: 'https://twitter.com/koteiou',
+      external: true,
+    },
+    {
+      name: '🎥 Twitch',
+      url: 'https://www.twitch.tv/yuitaaaaa',
+      external: true,
+    },
+    {
+      name: '📼 YouTube',
+      url: 'https://www.youtube.com/@yuita',
+      external: true,
+    },
+    {
+      name: '🏆 speedrun.com',
+      url: 'https://www.speedrun.com/ja-JP/users/yuita_',
+      external: true,
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
+  avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'yuita',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
+    // Visit https://icones.js.org/ for icon codes
+    // `pnpm add @iconify-json/<icon-set-name>`
     {
       name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      icon: 'fa6-brands:twitter',
+      url: 'https://twitter.com/koteiou',
+    },
+    {
+      name: 'Twitch',
+      icon: 'fa6-brands:twitch',
+      url: 'https://www.twitch.tv/yuitaaaaa',
+    },
+    {
+      name: 'YouTube',
+      icon: 'fa6-brands:youtube',
+      url: 'https://www.youtube.com/@yuita',
     },
     {
       name: 'Steam',
       icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
-    {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://steamcommunity.com/profiles/76561199572949580/',
     },
   ],
 }
 
 export const licenseConfig: LicenseConfig = {
-  enable: true,
+  enable: false,
   name: 'CC BY-NC-SA 4.0',
   url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
 }
